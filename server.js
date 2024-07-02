b5b9fcaad8c1364d370821bca1855cf9f695919f
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-07-02 02:07:16 +0100

    chnages

--- server.js
+++ server.js
@@ -1,57 +1,67 @@
 import express from 'express';
+import requestIp from 'request-ip';
 import axios from 'axios';
+import geoip from 'geoip-lite';
 import dotenv from 'dotenv';
+
+dotenv.config()
+
 const app = express();
-dotenv.config();
+const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const apiWeatherKey = process.env.apikey1 || '3fc121facde2e3444b75cfe8163afcc9';
 
-const PORT = process.env.PORT || 3000;
-const apiLocationKey = process.env.apikey;
-const apiWeatherKey = process.env.apikey1;
+
+app.use(requestIp.mw());
+
+const cleanIPv6 = (ip) => {
+  if (ip && ip.startsWith('::ffff:')) {
+    return ip.slice(7);
+  }
+  return ip;
+};
+
+
+const cleanVisitorName = (name) => {
+  return name.replace(/[\\/"]/g, '');
+};
+
 
 app.get('/api/hello', async (req, res) => {
-  const clientName = req.query.visitor_name;
-  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(req.header)
-  console.log(clientIp);
+  const visitor_name = cleanVisitorName(req.query.visitor_name || 'Guest');
+  const client_ip = cleanIPv6(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+  const geo = geoip.lookup(client_ip);
+  
+  const ipLocation = geo?.city;
+  console.log(ipLocation);
 
-  try {
-    // Get location data based on client's IP address
-    const locationResponse = await axios.get(`https://ipinfo.io/${clientIp}?token=${apiLocationKey}`)
-    .then(response => {
-      console.log(response.data.city);
-      return response.data.city;
-    })  
-      .catch(error => {
-        console.error('Error fetching location data:', error.message);
-        throw new Error('Location data not available');
-      });
+  // let location = 'Location information not available';
+  // if (geo && geo.city) {
+  //   location = geo.city;
+  // }
 
-    const weatherResponse = await axios.get(`http://api.weatherstack.com/current?access_key=${apiWeatherKey}&query=${locationResponse}`)
+  const weatherResponse = await axios.get(`http://api.weatherstack.com/current?access_key=${apiWeatherKey}&query=${ipLocation}`)
       .then(response => {
-        console.log(response.data.current.temperature);
-        return response.data.current.temperature;
+        console.log(response.data);
+        return response;
       })
       .catch(error => {
         console.error('Error fetching weather data:', error.message);
         throw new Error('Weather data not available');
       });
 
-    let temperature = weatherResponse;
+      //const temperature = weatherResponse.da;
+      //console.log(temperature);
 
-    res.json({
-      client_ip: clientIp,
-      location: locationResponse,
-      greetings: `Hello, ${clientName}! It's ${parseInt(temperature)} degrees Celcius in ${locationResponse}.`,
-    });
+  const response = `{
+    "client_ip": "${client_ip}",
+    "location": "${ipLocation}",
+    "greeting": "Hello, ${visitor_name}!"
+  }`;
 
-  } catch (err) {
-    console.error('Error fetching data:', err.message);
-    res.status(500).send('Server Error');
-  }
+  res.type('json').send(response);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
