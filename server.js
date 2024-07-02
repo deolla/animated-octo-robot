d67f5bc5fdd6c498922b0a86f44eb9d2a9704ce2
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-07-02 09:43:10 +0100

    updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import requestIp from 'request-ip';
 import axios from 'axios';
-import geoip from 'geoip-lite';
+// import geoip from 'geoip-lite';
 import dotenv from 'dotenv';
 
 dotenv.config()
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express();
 const PORT = 3000;
 
-const apiWeatherKey = process.env.apikey1 || '3fc121facde2e3444b75cfe8163afcc9';
+const apiWeatherKey = process.env.apikey1;
 
 
 app.use(requestIp.mw());
@@ -21,7 +21,6 @@ const cleanIPv6 = (ip) => {
   return ip;
 };
 
-
 const cleanVisitorName = (name) => {
   return name.replace(/[\\/"]/g, '');
 };
@@ -29,37 +28,30 @@ const cleanVisitorName = (name) => {
 
 app.get('/api/hello', async (req, res) => {
   const visitor_name = cleanVisitorName(req.query.visitor_name || 'Guest');
-  const client_ip = cleanIPv6(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-  const geo = geoip.lookup(client_ip);
-  
-  const ipLocation = geo?.city;
-  console.log(ipLocation);
+  const client_ip = cleanIPv6(req.headers['x-forwarded-for'] || req.clientIp || '127.0.0.1' || req.connection.remoteAddress);
 
-  // let location = 'Location information not available';
-  // if (geo && geo.city) {
-  //   location = geo.city;
-  // }
+  console.log(client_ip);
 
-  const weatherResponse = await axios.get(`http://api.weatherstack.com/current?access_key=${apiWeatherKey}&query=${ipLocation}`)
-      .then(response => {
-        console.log(response.data);
-        return response;
-      })
-      .catch(error => {
-        console.error('Error fetching weather data:', error.message);
-        throw new Error('Weather data not available');
-      });
-
-      //const temperature = weatherResponse.da;
-      //console.log(temperature);
-
-  const response = `{
-    "client_ip": "${client_ip}",
-    "location": "${ipLocation}",
-    "greeting": "Hello, ${visitor_name}!"
-  }`;
-
-  res.type('json').send(response);
+  try {
+    const weatherResponse = await axios.get(`https://api.weatherapi.com/v1/current.json`, {
+      params: {
+        key: apiWeatherKey,
+        q: client_ip,
+      },
+    });
+    
+    const ipLocation = weatherResponse.data.location.name;
+    
+    const response = {
+      client_ip: client_ip,
+      location: ipLocation,
+      greeting: `Hello, ${visitor_name}!`,
+    };
+    res.json(response);
+  } catch (error) {
+    console.error(`Error processing request: ${error.message}`);
+    res.status(500).json({ error: 'Failed to fetch weather data' });
+  }
 });
 
 
